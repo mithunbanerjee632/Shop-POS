bd86f2ae8594abaa18ea5413286cbad5c320afbc
Mithun Banerjee <34417291+mithunbanerjee632@users.noreply.github.com>
2022-07-17 11:51:04 +0600

    Create API For Category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,9 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CategoryModel;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class CategoryController extends Controller
 {
-    //
+   function AddCategory(Request $request){
+       $catName = $request->input('catName');
+       $imagePath = $request->file('image')->store('public');
+       $catCode = time();
+
+       $result = CategoryModel::insert([
+           'cat_name'=>$catName,
+           'cat_code'=>$catCode,
+           'cat_icon'=>$imagePath
+       ]);
+
+       return $result;
+   }
+
+   function SelectCategory(){
+       $result = CategoryModel::all();
+       return $result;
+   }
+
+   function DeleteCategory(Request $request){
+       $id = $request->id;
+       $imageName = CategoryModel::where('id',$id)->get('cat_icon');
+       Storage::delete($imageName[0]['cat_icon']);
+       $result = CategoryModel::where('id',$id)->delete();
+       return $result;
+
+   }
+
+   function UpdateCategoryWithImage(Request $request){
+       $id = $request->input('id');
+       $name = $request->input('name');
+
+       //New Image Upload
+       $imagePath = $request->file('image')->store('public');
+
+       //Old Image Delete
+       $imageName = CategoryModel::where('id',$id)->get('cat_icon');
+       Storage::delete($imageName[0]['cat_icon']);
+
+       $result = CategoryModel::where('id',$id)->update(['cat_name'=>$name,'cat_icon'=>$imagePath]);
+       return $result;
+   }
+
+    function UpdateCategoryWithoutImage(Request $request){
+       $id = $request->input('id');
+       $name = $request->input('name');
+
+       $result = CategoryModel::where('id',$id)->update(['cat_name'=>$name]);
+       return $result;
+    }
+
 }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,12 +15,19 @@ use App\Http\Controllers\UserController;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+//User Routes
 Route::post('/AddUser',[UserController::class,'AddUser']);
 Route::get('/DeleteUser/{id}',[UserController::class,'DeleteUser']);
 Route::get('/SelectUser',[UserController::class,'SelectUser']);
 Route::post('/UpdateUser',[UserController::class,'UpdateUser']);
 
-
+//Category Routes
+Route::post('/AddCategory',[CategoryController::class,'AddCategory']);
+Route::get('/SelectCategory',[CategoryController::class,'SelectCategory']);
+Route::get('/DeleteCategory/{id}',[CategoryController::class,'DeleteCategory']);
+Route::post('/UpdateCategoryWithImage',[CategoryController::class,'UpdateCategoryWithImage']);
+Route::post('/UpdateCategoryWithoutImage',[CategoryController::class,'UpdateCategoryWithoutImage']);
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
